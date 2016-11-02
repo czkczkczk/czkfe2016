2b1595fd0cdf3df7fd5ac6ea12e0abe7d4c4fc38
czkczkczk <1473059150qq.com>
2016-11-02 22:37:33 +0800

    全站开发

---
+++ jquery/全站开发/js/js.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2016/11/2 0002.
+ */
+$(function(){
+    var $this=$('input');
+$('input').on('focus',function(){
+
+    if(this.value == this.defaultValue){
+        this.value = ''
+    }
+}).on('blur',function(){
+    if(this.value==''){
+        this.value=this.defaultValue
+    }
+})
+    $('input').on('keypress',function(event){
+        if(event.keyCode==13){
+            $this.val($(this).val())
+        }
+    })
+    var index = 0;
+    $('#left').on('click', function () {
+        index-- ;
+        if(index<0){
+           index=1;
+        }
+        $('#img img').eq(index).show().siblings().hide()
+    })
+    $('#right').on('click',function(){
+        index++;
+        if(index==$('#img img').size()){
+            index=0;
+        }
+        $('#img img').eq(index).show().siblings().hide()
+    })
+})
